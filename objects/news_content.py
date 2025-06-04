9d63614f5c779547d164146ab1f5170bb2b81bb2
Anna Jacobsen <anna.holden.jacobsen@gmail.com>
2025-06-04 08:02:24 +0200

    change var name

--- objects/news_content.py
+++ objects/news_content.py
@@ -22,8 +22,8 @@ class NewsContent:
     
 def topic_likely_to_be_fake(topic_vector):
     """Determine if a topic vector is likely to be fake news."""
-    sum_of_first_three_elements = sum(topic_vector[:2])
-    return sum_of_first_three_elements > 0.3
+    sum_of_first_two_elements = sum(topic_vector[:2])
+    return sum_of_first_two_elements > 0.3
 
 def generate_news_content(fake_news_percentage, news_amount, creation_step):
     """Create a mix of real and fake news content based on the model's fake_news_percentage."""
