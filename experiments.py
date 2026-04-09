e1cce87febc454f62fd5af3364cefb77cb7b20a5
Iver Ringheim <iver.ringheim@gmail.com>
2026-04-09 14:44:56 +0200

    reduce repeated code

--- experiments.py
+++ experiments.py
@@ -127,7 +127,7 @@ def run_recommender_comparison_experiment(
         parameters=parameters,
         iterations=iterations,
         max_steps=max_steps,
-        number_processes=8,  # Set to higher number for parallel processing
+        number_processes=5,  # Set to higher number for parallel processing
         data_collection_period=1,  # Collect data at each step
         display_progress=True,
     )
@@ -298,8 +298,8 @@ if __name__ == "__main__":
     # Run the experiment
     results_df, model_data, summary_df, community_data_file = (
         run_recommender_comparison_experiment(
-            iterations=5,  # Number of runs per recommender type
-            max_steps=700,  # Steps per run
+            iterations=1,  # Number of runs per recommender type
+            max_steps=200,  # Steps per run
             n_agents=200,  # Number of agents
             m_links=8,  # Links per new node
             news_amount=400,  # Initial news items
